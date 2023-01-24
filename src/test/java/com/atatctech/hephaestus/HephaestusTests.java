01a5a6161b8d604a9f439b429bd7bea27c29176f
ATATC <futerry@outlook.com>
2023-01-24 00:59:25 -0500

    Bug found.

--- src/test/java/com/atatctech/hephaestus/HephaestusTests.java
+++ src/test/java/com/atatctech/hephaestus/HephaestusTests.java
@@ -1,11 +1,12 @@
 package com.atatctech.hephaestus;
 
 import com.atatctech.hephaestus.component.Skeleton;
+import com.atatctech.hephaestus.exception.BadFormat;
 import org.junit.jupiter.api.Test;
 
 public class HephaestusTests {
     @Test
-    void test() {
+    void test() throws BadFormat {
         Skeleton skeleton1 = new Skeleton("S1");
         Skeleton skeleton2 = new Skeleton("S2");
         Skeleton skeleton3 = new Skeleton("S3");
@@ -17,8 +18,7 @@ public class HephaestusTests {
         skeleton2.appendChild(skeleton4);
         skeleton3.appendChild(skeleton5);
 
-        skeleton1.forEach((component, depth) -> System.out.println(depth));
-
-        skeleton1.parallelTraversal((component, depth) -> System.out.println(depth));
+        // FixMe
+        System.out.println(Hephaestus.parseExpr(skeleton1.expr()));
     }
 }
