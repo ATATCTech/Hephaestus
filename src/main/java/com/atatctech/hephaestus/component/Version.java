e593f775d611efac7dab088c419e6c5d86e25637
ATATC <futerry@outlook.com>
2023-03-08 19:25:15 -0500

    GitHub repository url changed to https://github.com/ATATCTech/Hephaestus.

---
+++ src/main/java/com/atatctech/hephaestus/component/Version.java
@@ -0,0 +1,65 @@
+package com.atatctech.hephaestus.component;
+
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+@ComponentConfig(tagName = "v")
+public class Version extends WrapperComponent {
+    public static class Serial {
+        @NotNull
+        protected final Object[] args;
+
+        public Serial(Object... args) {
+            this.args = args;
+        }
+
+        public boolean equals(Serial serial, boolean sequential) {
+            if (args.length != serial.args.length) return false;
+            List<Object> argsList = Arrays.asList(serial.args);
+            for (int i = 0; i < args.length; i++) {
+                if (sequential) if (!Objects.equals(args[i], serial.args[i])) return false;
+                else if (!argsList.contains(args[i])) return false;
+            }
+            return true;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Serial serial)) return false;
+            return equals(serial, true);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(args);
+        }
+    }
+
+    protected Serial serial;
+
+    public Version(Serial serial) {
+        setSerial(serial);
+    }
+
+    public Version(String serial) {
+        setSerial(new Serial(serial));
+    }
+
+    public void setSerial(Serial serial) {
+        this.serial = serial;
+    }
+
+    public Serial getSerial() {
+        return serial;
+    }
+
+    @Override
+    public String expr() {
+        return null;
+    }
+}
