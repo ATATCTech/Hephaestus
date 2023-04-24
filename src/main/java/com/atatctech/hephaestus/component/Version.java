aad3d0f2e553d3f90096f63e49ef58c4f9547002
ATATC <futerry@outlook.com>
2023-04-24 11:39:46 -0400

    Routine updates.

--- src/main/java/com/atatctech/hephaestus/component/Version.java
+++ src/main/java/com/atatctech/hephaestus/component/Version.java
@@ -1,28 +1,31 @@
 package com.atatctech.hephaestus.component;
 
 
+import com.atatctech.hephaestus.attribute.Attribute;
 import com.atatctech.hephaestus.parser.Parser;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 @ComponentConfig(tagName = "v")
 public class Version extends WrapperComponent {
-    public static class Serial {
+    public static class Serial implements Serializable {
         protected final @NotNull Object[] args;
 
         public Serial(Object... args) {
             this.args = args;
         }
 
-        public boolean equals(@NotNull Serial serial, boolean sequential) {
-            if (args.length != serial.args.length) return false;
-            List<Object> argsList = Arrays.asList(serial.args);
+        public boolean equals(@NotNull Serial o, boolean sequential) {
+            if (args.length != o.args.length) return false;
+            List<Object> argsList = Arrays.asList(o.args);
             for (int i = 0; i < args.length; i++) {
-                if (sequential) if (!Objects.equals(args[i], serial.args[i])) return false;
-                else if (!argsList.contains(args[i])) return false;
+                if (sequential) {
+                    if (!Objects.equals(args[i], o.args[i])) return false;
+                } else if (!argsList.contains(args[i])) return false;
             }
             return true;
         }
@@ -30,8 +33,8 @@ public class Version extends WrapperComponent {
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
-            if (!(o instanceof Serial serial)) return false;
-            return equals(serial, true);
+            if (o instanceof Serial serial) return equals(serial, true);
+            return false;
         }
 
         @Override
@@ -43,29 +46,25 @@ public class Version extends WrapperComponent {
     public static Parser<Version> PARSER;
 
     static {
-        PARSER = expr -> null;
+        PARSER = WrapperComponent.makeParser(Version.class);
     }
 
-    protected Serial serial;
+    @Attribute
+    protected @NotNull Serial serial = new Serial("undefined");
 
-    public Version(Serial serial) {
+    public Version(@NotNull Serial serial) {
         setSerial(serial);
     }
 
-    public Version(String serial) {
+    public Version(@NotNull String serial) {
         setSerial(new Serial(serial));
     }
 
-    public void setSerial(Serial serial) {
+    public void setSerial(@NotNull Serial serial) {
         this.serial = serial;
     }
 
-    public Serial getSerial() {
+    public @NotNull Serial getSerial() {
         return serial;
-    }
-
-    @Override
-    public @NotNull String expr() {
-        return "";
     }
 }
