070a6d1b26fa6551b5cfdb768b92d4441ed93414
ATATC <futerry@outlook.com>
2023-04-27 22:43:11 -0400

    Routine updates.

--- src/main/java/com/atatctech/hephaestus/exception/MissingMethodException.java
+++
@@ -1,9 +0,0 @@
-package com.atatctech.hephaestus.exception;
-
-import org.jetbrains.annotations.NotNull;
-
-public class MissingMethodException extends HephaestusRuntimeException {
-    public MissingMethodException(@NotNull Class<?> clz, @NotNull String methodName) {
-        super("Missing method " + methodName + " in " + clz.getName() + ".");
-    }
-}
